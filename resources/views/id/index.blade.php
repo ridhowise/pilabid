60ecc4f1a6c09d8af85ef1c3d24ee3a51c7b4a89
ridhowise <ridhowise1@gmail.com>
2021-07-23 21:18:52 +0800

    clear

--- resources/views/id/index.blade.php
+++ resources/views/id/index.blade.php
@@ -2,35 +2,28 @@
 <html>
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="{{ URL::asset('adm/css/id.css') }}" rel="stylesheet">
     <title>Id Maesalab</title>
 </head>
 <body>
 
 <div class="text-center" style="margin-top: 50px;">
+<div class="card">
+        <div class="card-img">
+        <img src="{{ URL::asset('images/') }}/{{$data->images}}"></div>
+        <div class="desc">
+            <h6 class="primary-text">{{$data->name}}</h6>
 
-    
+            <h6 class="secondary-text">{{$data->level->name}} </h6>
+           <br>
+            {!! QrCode::size(100)->generate("id.maesalab.net/id/$data->id"); !!}
+
+        </div>
+
+
+
 
 </div>
-
-<div class="w3-container">
-
-  <div class="w3-card-5 " style="width:50%;margin:auto;background-image: linear-gradient(180deg, #6237a0, white);">
-
-    <div class="w3-container w3-center">
-          <h3>{{$data->level->name}}</h3>
-        <img style="margin:auto;object-fit: cover;border-radius: 50%;height: 200px;width: 200px;border: 5px solid black;" src="{{ URL::asset('images/') }}/{{$data->images}}" alt="Avatar">
-
-      <h5>{{$data->name}}</h5>
-      {!! QrCode::size(100)->generate("id.maesalab.net/id/$data->id"); !!}
-
-      <div class="w3-section" style="height:10px">
-        
-      </div>
-
-    </div>
-
-  </div>
 </div>
 
 </body>
